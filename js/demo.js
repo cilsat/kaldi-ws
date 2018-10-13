e2c7f0ae80135bcb951775c8f3cf4c0f0c4e3707
Cil Satriawan <cil.satriawan@gmail.com>
2018-10-13 16:55:19 +0700

    Separate ubtech-demo to dev branch

--- js/demo.js
+++ js/demo.js
@@ -8,133 +8,133 @@
 //       and event callbacks onResults, onError, ...
 
 var Transcription = function(cfg) {
-    var index = 0;
-    var list = [];
+  var index = 0;
+  var list = [];
 
-    this.add = function(text, isFinal) {
-        list[index] = text;
-        if (isFinal) {
-            index++;
-        }
+  this.add = function(text, isFinal) {
+    list[index] = text;
+    if (isFinal) {
+      index++;
     }
+  }
 
-    this.toString = function() {
-        return list.join('. ');
-    }
+  this.toString = function() {
+    return list.join('. ');
+  }
 };
 
 var tt = new Transcription();
 
 var cfgDictate = {
-    onPartialResults : function(hypos) {
-        tt.add(hypos[0].transcript, false);
-        __updateTranscript(tt.toString());
-    },
-    onResults : function(hypos) {
-        tt.add(hypos[0].transcript, true);
-        __updateTranscript(tt.toString());
-    }
+  onPartialResults : function(hypos) {
+    tt.add(hypos[0].transcript, false);
+    __updateTranscript(tt.toString());
+  },
+  onResults : function(hypos) {
+    tt.add(hypos[0].transcript, true);
+    __updateTranscript(tt.toString());
+  }
 };
 
 var dictate = new Dictate(cfgDictate);
 
 // Private methods (callbacks)
 function __updateTranscript(text) {
-    $("#trans").val(text);
-    $('#trans').scrollTop($('#trans')[0].scrollHeight);
+  $("#trans").val(text);
+  $('#trans').scrollTop($('#trans')[0].scrollHeight);
 }
 
 // Public methods (called from the GUI)
 function toggleMic() {
-    if (!$('#buttonMic').hasClass('disabled')) {
-        $('#buttonMic').toggleClass('active');
-        $('#buttonFile').toggleClass('disabled');
-        if ($('#buttonMic').hasClass('active')) {
-            dictate.record();
-            $('#textStart').addClass('hidden');
-            $('#textMic').removeClass('hidden');
-        } else {
-            dictate.stop();
-            $('#textStart').removeClass('hidden');
-            $('#textMic').addClass('hidden');
-        }
+  if (!$('#buttonMic').hasClass('disabled')) {
+    $('#buttonMic').toggleClass('active');
+    $('#buttonFile').toggleClass('disabled');
+    if ($('#buttonMic').hasClass('active')) {
+      dictate.record();
+      $('#textStart').addClass('hidden');
+      $('#textMic').removeClass('hidden');
+    } else {
+      dictate.stop();
+      $('#textStart').removeClass('hidden');
+      $('#textMic').addClass('hidden');
     }
+  }
 }
 
 function toggleFile() {
-    if (!$('#buttonFile').hasClass('disabled')) {
-        $('#buttonFile').toggleClass('active');
-        $('#buttonMic').toggleClass('disabled');
-        $('#textStart').toggleClass('hidden');
-        $('#textFile').toggleClass('hidden');
-        $('#buttonBrowse').toggleClass('hidden');
-        $('#buttonSend').toggleClass('hidden');
-        $('#buttonPlay').toggleClass('hidden');
-        $('#infoFile').toggleClass('hidden');
+  if (!$('#buttonFile').hasClass('disabled')) {
+    $('#buttonFile').toggleClass('active');
+    $('#buttonMic').toggleClass('disabled');
+    $('#textStart').toggleClass('hidden');
+    $('#textFile').toggleClass('hidden');
+    $('#buttonBrowse').toggleClass('hidden');
+    $('#buttonSend').toggleClass('hidden');
+    $('#buttonPlay').toggleClass('hidden');
+    $('#infoFile').toggleClass('hidden');
 
-        if ($('#buttonFile').hasClass('active')) {
-            dictate.cancel();
-        }
+    if ($('#buttonFile').hasClass('active')) {
+      dictate.cancel();
     }
+  }
 }
 
 function handleFile() {
-    var file = document.getElementById('audioFile').files[0];
-    if (file) {
-        var info;
-        if (file.type == 'audio/x-wav' || file.type == 'audio/wav') {
-            $('#buttonSend').removeClass('disabled');
-            $('#buttonPlay').removeClass('disabled');
-            info = file.name;
-        } else {
-            $('#buttonSend').addClass('disabled');
-            $('#buttonPlay').addClass('disabled');
-            info = 'Unsupported format ' + file.type;
-        }
-        $('#infoFile').html(info);
+  var file = document.getElementById('audioFile').files[0];
+  if (file) {
+    var info;
+    if (file.type == 'audio/x-wav' || file.type == 'audio/wav') {
+      $('#buttonSend').removeClass('disabled');
+      $('#buttonPlay').removeClass('disabled');
+      info = file.name;
     } else {
-        console.log($('#audioFile').val());
+      $('#buttonSend').addClass('disabled');
+      $('#buttonPlay').addClass('disabled');
+      info = 'Unsupported format ' + file.type;
     }
+    $('#infoFile').html(info);
+  } else {
+    console.log($('#audioFile').val());
+  }
 }
 
 function sendFile() {
-    if (!$('#buttonSend').hasClass('disabled')) {
-        dictate.send($('#audioFile').get(0).files[0]);
-    }
+  if (!$('#buttonSend').hasClass('disabled')) {
+    dictate.send($('#audioFile').get(0).files[0]);
+  }
 }
 
 function playFile() {
-    if (!$('#buttonPlay').hasClass('disabled')) {
-        var file = $('#audioFile').get(0).files[0];
-        var wavBlob = new Blob([file]);
-        var wavURL = window.URL.createObjectURL(wavBlob);
-        var audio = new Audio();
-        audio.src = wavURL;
-        audio.play();
-    }
+  if (!$('#buttonPlay').hasClass('disabled')) {
+    var file = $('#audioFile').get(0).files[0];
+    var wavBlob = new Blob([file]);
+    var wavURL = window.URL.createObjectURL(wavBlob);
+    var audio = new Audio();
+    audio.src = wavURL;
+    audio.play();
+  }
 }
 
 function saveTranscript() {
-    var text = $('#trans').val();
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', 'transcription.txt');
-    element.style.display = 'none';
-    document.body.appendChild(element);
+  var text = $('#trans').val();
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', 'transcription.txt');
+  element.style.display = 'none';
+  document.body.appendChild(element);
 
-    element.click();
-    document.body.removeChild(element);
+  element.click();
+  document.body.removeChild(element);
 };
 
 function clearTranscript() {
-    tt = new Transcription();
-    $("#trans").val("");
+  tt = new Transcription();
+  $("#trans").val("");
 }
 
 window.onload = function() {
-    clearTranscript();
-    if (!window.AudioContext || !navigator.mediaDevices.getUserMedia) {
-        $('#textBrowser').removeClass('hidden');
-    }
+  clearTranscript();
+  if (!window.AudioContext || !navigator.mediaDevices.getUserMedia) {
+    $('#textBrowser').removeClass('hidden');
+  }
 };
 
--- js/dictate.js
+++ js/dictate.js
@@ -1,289 +1,215 @@
 'use strict';
 
 function Dictate(_config) {
-    var configDefault = {
-        server: 'ws://localhost:8080',
-        serverSpeech: '/client/ws/speech',
-        serverStatus: '/client/ws/status',
-        contentType: 'content-type=audio/x-raw,+layout=(string)interleaved,+rate=(int)16000,+format=(string)S16LE,+channels=(int)1',
-        bufferSize: 8192,
-        inputChannels: 1,
-        outputChannels: 1
-    };
+  var configDefault = {
+    server: 'ws://localhost:6667/query',
+    bufferSize: 8192,
+    inputChannels: 1,
+    outputChannels: 1
+  };
 
-    var config = _config;
+  var config = _config;
 
-    // SERVER SETTINGS
-    this.server = config.server || configDefault.server
-    this.serverSpeech = config.serverSpeech || configDefault.serverSpeech;
-    this.serverStatus = config.serverStatus || configDefault.serverStatus;
+  // SERVER SETTINGS
+  this.server = config.server || configDefault.server
 
-    // AUDIO SETTINGS
-    this.contentType = config.contentType || configDefault.contentType;
-    this.bufferSize = config.bufferSize || configDefault.bufferSize;
-    this.inputChannels = config.inputChannels || configDefault.inputChannels;
-    this.outputChannels = config.outputChannels || configDefault.outputChannels;
-    this.sampleRate = 16000;
+  // AUDIO SETTINGS
+  this.bufferSize = config.bufferSize || configDefault.bufferSize;
+  this.inputChannels = config.inputChannels || configDefault.inputChannels;
+  this.outputChannels = config.outputChannels || configDefault.outputChannels;
+  this.sampleRate = 16000;
 
-    // FILE VARIABLES
-    this.sending = false;
-    this.samplesOffset = 0;
-    this.chunkSize = 8000;
-    this.rate = 8;
+  // MICROPHONE VARIABLES
+  this.recording = false;
+  this.requestedAccess = false;
+  this.bufferUnusedSamples = new Float32Array(0);
+  this.samplesAll = new Float32Array(20000000);
+  this.samplesAllOffset = 0;
 
-    // FILE METHODS
-    this.send = function(file) {
-        this.contentType = 'content-type=';
-        this.ws = this.createWebSocket();
-        this.audioFile = new Blob([file]);
-        this.samplesOffset = 0;
-        this.sending = true;
+  // MICROPHONE METHODS
+  this.record = function() {
+    if (!navigator.mediaDevices.getUserMedia || this.requestedAccess)
+      return;
 
-        this.readChunks();
-    };
+    this.requestedAccess = true;
+    console.log('Init recording');
+    navigator.mediaDevices.getUserMedia({audio: true})
+      .then(this.onMediaStream.bind(this))
+      .catch(this.onPermissionRejected.bind(this));
+  };
 
-    this.cancel = function() {
-        this.sending = false;
-        if (this.ws) {
-            this.closeWebSocket();
-            this.ws = null;
-        }
-    };
+  this.onMediaStream = function(stream) {
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
 
-    this.readChunks = function() {
-        var reader = new FileReader();
-        var chunk = this.audioFile.slice(this.samplesOffset,
-            this.samplesOffset + this.chunkSize);
-        reader.onload = this.onLoadChunk.bind(this);
-        reader.readAsArrayBuffer(chunk);
-    };
+    console.log('Handling media stream');
+    if (!AudioContext)
+      console.log('AudioContext unavailable');
 
-    this.onLoadChunk = function(e) {
-        if (this.samplesOffset >= this.audioFile.size) {
-            console.log('file end');
-            // we don't need to close websocket connection manually
-            // let server close websocket when transcription has completed
-            return;
-        }
+    if (!this.audioContext)
+      this.audioContext = new AudioContext();
 
-        if (e.target.error == null) {
-            var buffer = e.target.result;
-            var state = this.ws.readyState;
-            if (state == 1) {
-                //console.log('ws blob sent ' + buffer);
-                this.ws.send(buffer);
-                this.samplesOffset += buffer.byteLength;
-            } else {
-                console.log('ws readyState ' + state);
-            }
-        } else {
-            console.log(e.target.error);
-        }
-        // delay next read chunk according to rate
-        window.setTimeout(this.readChunks.bind(this), 1000/this.rate);
-    };
+    var gain = this.audioContext.createGain();
+    var audioInput = this.audioContext.createMediaStreamSource(stream);
 
-    // MICROPHONE VARIABLES
-    this.recording = false;
+    audioInput.connect(gain);
+
+    if(!this.mic) {
+      this.mic = this.audioContext.createScriptProcessor(this.bufferSize,
+        this.inputChannels, this.outputChannels);
+    }
+
+    this.ws = this.createWebSocket();
+    this.sampleRate = this.audioContext.sampleRate;
+
+    this.mic.onaudioprocess = this._onaudioprocess.bind(this);
+    this.stream = stream;
+
+    gain.connect(this.mic);
+    this.mic.connect(this.audioContext.destination);
+    this.recording = true;
     this.requestedAccess = false;
-    this.bufferUnusedSamples = new Float32Array(0);
-    this.samplesAll = new Float32Array(20000000);
-    this.samplesAllOffset = 0;
+  };
 
-    // MICROPHONE METHODS
-    this.record = function() {
-        if (!navigator.mediaDevices.getUserMedia || this.requestedAccess)
-            return;
+  this._onaudioprocess = function(data) {
+    if (!this.recording) return;
+    var chan = data.inputBuffer.getChannelData(0);
+    this.saveData(new Float32Array(chan));
+    this.onAudio(this._exportDataBufferTo16Khz(new Float32Array(chan)));
+  };
 
-        this.requestedAccess = true;
-        console.log('init recording');
-        navigator.mediaDevices.getUserMedia({audio: true})
-            .then(this.onMediaStream.bind(this))
-            .catch(this.onPermissionRejected.bind(this));
+  this.onAudio = function(blob) {
+    if (this.ws) {
+      var state = this.ws.readyState;
+      if (state == 1) {
+        console.log('WS send blob ' + blob);
+        this.ws.send(blob);
+      } else {
+        console.log('WS network error');
+      }
+    }
+  };
+
+  this.onPermissionRejected = function(e) {
+    this.requestedAccess = false;
+    console.log(e);
+  };
+
+  this.onError = function(e) {
+    config.onError(ERR_CLIENT, e);
+  };
+
+  this.stop = function() {
+    if (!this.recording) return;
+    this.recording = false;
+    this.stream.getTracks()[0].stop();
+    this.requestedAccess = false;
+    this.mic.disconnect(0);
+    if (this.audioContext) {
+      this.audioContext.close();
+      this.audioContext = null;
+    }
+    this.closeWebSocket();
+  };
+
+  this.closeWebSocket = function() {
+    if (this.ws) {
+      this.ws.close();
+      this.ws = null;
+    }
+  };
+
+  // UTILITY METHODS
+  this.createWebSocket = function() {
+    console.log('WS connecting');
+    var url = this.server;
+    var ws = new WebSocket(url);
+
+    ws.onmessage = function(msg) {
+      var data = msg.data;
+      if (!(data instanceof Object) || !(data instanceof Blob)) {
+        var res = window.JSON.parse(data);
+        console.log(res.text);
+      } else {
+        console.log('WS cannot parse msg');
+      }
     };
 
-    this.onMediaStream = function(stream) {
-        var AudioContext = window.AudioContext || window.webkitAudioContext;
-
-        console.log('handling media stream');
-        if (!AudioContext)
-            console.log('AudioContext unavailable');
-
-        if (!this.audioContext)
-            this.audioContext = new AudioContext();
-
-        var gain = this.audioContext.createGain();
-        var audioInput = this.audioContext.createMediaStreamSource(stream);
-
-        audioInput.connect(gain);
-
-        if(!this.mic) {
-            this.mic = this.audioContext.createScriptProcessor(this.bufferSize,
-                this.inputChannels, this.outputChannels);
-        }
-
-        this.ws = this.createWebSocket();
-        this.sampleRate = this.audioContext.sampleRate;
-
-        this.mic.onaudioprocess = this._onaudioprocess.bind(this);
-        this.stream = stream;
-
-        gain.connect(this.mic);
-        this.mic.connect(this.audioContext.destination);
-        this.recording = true;
-        this.requestedAccess = false;
+    ws.onclose = function(msg) {
+      var code = msg.code;
+      var reason = msg.reason;
+      var wasClean = msg.wasClean;
+      console.log('ws closed: ' + code + '/' + reason + '/' + wasClean);
     };
 
-    this._onaudioprocess = function(data) {
-        if (!this.recording) return;
-        var chan = data.inputBuffer.getChannelData(0);
-        this.saveData(new Float32Array(chan));
-        this.onAudio(this._exportDataBufferTo16Khz(new Float32Array(chan)));
+    ws.onerror = function(e) {
+      console.log('ws error: ' + e.data)
     };
 
-    this.onAudio = function(blob) {
-        if (this.ws) {
-            var state = this.ws.readyState;
-            if (state == 1) {
-                console.log('ws send blob ' + blob);
-                this.ws.send(blob);
-            } else {
-                console.log('ws network error');
-            }
-        }
-    };
+    return ws;
+  };
 
-    this.onPermissionRejected = function(e) {
-        this.requestedAccess = false;
-        console.log('rejected ' + e);
-        //config.onError(ERR_CLIENT, 'Permission to access microphone rejected');
-    };
+  this.saveData = function(samples) {
+    for (var i = 0; i < samples.length; ++i) {
+      this.samplesAll[this.samplesAllOffset + i] = samples[i];
+    }
+    this.samplesAllOffset += samples.length;
+    //console.log('samples: ' + this.samplesAllOffset);
+  };
 
-    this.onError = function(e) {
-        config.onError(ERR_CLIENT, e);
-    };
+  this._exportDataBufferTo16Khz = function(bufferNewSamples) {
+    var buffer = null;
+    var newSamples = bufferNewSamples.length;
+    var unusedSamples = this.bufferUnusedSamples.length;
+    var i;
 
-    this.stop = function() {
-        if (!this.recording) return;
-        this.recording = false;
-        this.stream.getTracks()[0].stop();
-        this.requestedAccess = false;
-        this.mic.disconnect(0);
-        if (this.audioContext) {
-            this.audioContext.close();
-            this.audioContext = null;
-        }
-        this.closeWebSocket();
-    };
+    if (unusedSamples > 0) {
+      buffer = new Float32Array(unusedSamples + newSamples);
+      for (i = 0; i < unusedSamples; ++i) {
+        buffer[i] = this.bufferUnusedSamples[i];
+      }
+      for (i = 0; i < newSamples; ++i) {
+        buffer[unusedSamples + i] = bufferNewSamples[i];
+      }
+    } else {
+      buffer = bufferNewSamples;
+    }
 
-    this.closeWebSocket = function() {
-        if (this.ws) {
-            this.ws.close();
-            this.ws = null;
-        }
-    };
+    var filter = [
+      -0.037935, -0.00089024, 0.040173, 0.019989, 0.0047792, -0.058675,
+      -0.056487, -0.0040653, 0.14527, 0.26927, 0.33913, 0.26927, 0.14527,
+      -0.0040653, -0.056487, -0.058675, 0.0047792, 0.019989, 0.040173,
+      -0.00089024, -0.037935
+    ];
+    var samplingRateRatio = this.sampleRate / 16000;
+    var nOutputSamples = Math.floor((buffer.length - filter.length) / (samplingRateRatio)) + 1;
+    var pcmEncodedBuffer16k = new ArrayBuffer(nOutputSamples * 2);
+    var dataView16k = new DataView(pcmEncodedBuffer16k);
+    var index = 0;
+    var volume = 0x7FFF;
+    var nOut = 0;
 
-    // UTILITY METHODS
-    this.createWebSocket = function() {
-        console.log('ws connecting');
-        var url = this.server + this.serverSpeech + '?' + this.contentType;
-        var ws = new WebSocket(url);
+    for (i = 0; i + filter.length - 1 < buffer.length; i = Math.round(samplingRateRatio * nOut)) {
+      var sample = 0;
+      for (var j = 0; j < filter.length; ++j) {
+        sample += buffer[i + j] * filter[j];
+      }
+      sample *= volume;
+      dataView16k.setInt16(index, sample, true); // 'true' -> little endian
+      index += 2;
+      nOut++;
+    }
 
-        ws.onmessage = function(msg) {
-            var data = msg.data;
-            if (!(data instanceof Object) || !(data instanceof Blob)) {
-                var res = window.JSON.parse(data);
-                if (res.status == 0) {
-                    if (res.result.final) {
-                        console.log('final: ' + res.result.hypotheses[0].transcript);
-                        config.onResults(res.result.hypotheses);
-                    } else {
-                        config.onPartialResults(res.result.hypotheses);
-                    }
-                } else {
-                    console.log('ws error status ' + res.status);
-                }
-            } else {
-                console.log('ws cannot parse msg');
-            }
-        };
+    var indexSampleAfterLastUsed = Math.round(samplingRateRatio * nOut);
+    var remaining = buffer.length - indexSampleAfterLastUsed;
+    if (remaining > 0) {
+      this.bufferUnusedSamples = new Float32Array(remaining);
+      for (i = 0; i < remaining; ++i) {
+        this.bufferUnusedSamples[i] = buffer[indexSampleAfterLastUsed + i];
+      }
+    } else {
+      this.bufferUnusedSamples = new Float32Array(0);
+    }
 
-        ws.onclose = function(msg) {
-            var code = msg.code;
-            var reason = msg.reason;
-            var wasClean = msg.wasClean;
-            console.log('ws closed: ' + code + '/' + reason + '/' + wasClean);
-        };
-
-        ws.onerror = function(e) {
-            console.log('ws error: ' + e.data)
-        };
-
-        return ws;
-    };
-
-    this.saveData = function(samples) {
-        for (var i = 0; i < samples.length; ++i) {
-            this.samplesAll[this.samplesAllOffset + i] = samples[i];
-        }
-        this.samplesAllOffset += samples.length;
-        //console.log('samples: ' + this.samplesAllOffset);
-    };
-
-    this._exportDataBufferTo16Khz = function(bufferNewSamples) {
-        var buffer = null;
-        var newSamples = bufferNewSamples.length;
-        var unusedSamples = this.bufferUnusedSamples.length;
-        var i;
-
-        if (unusedSamples > 0) {
-            buffer = new Float32Array(unusedSamples + newSamples);
-            for (i = 0; i < unusedSamples; ++i) {
-                buffer[i] = this.bufferUnusedSamples[i];
-            }
-            for (i = 0; i < newSamples; ++i) {
-                buffer[unusedSamples + i] = bufferNewSamples[i];
-            }
-        } else {
-            buffer = bufferNewSamples;
-        }
-
-        var filter = [
-            -0.037935, -0.00089024, 0.040173, 0.019989, 0.0047792, -0.058675,
-            -0.056487, -0.0040653, 0.14527, 0.26927, 0.33913, 0.26927, 0.14527,
-            -0.0040653, -0.056487, -0.058675, 0.0047792, 0.019989, 0.040173,
-            -0.00089024, -0.037935
-        ];
-        var samplingRateRatio = this.sampleRate / 16000;
-        var nOutputSamples = Math.floor((buffer.length - filter.length) / (samplingRateRatio)) + 1;
-        var pcmEncodedBuffer16k = new ArrayBuffer(nOutputSamples * 2);
-        var dataView16k = new DataView(pcmEncodedBuffer16k);
-        var index = 0;
-        var volume = 0x7FFF;
-        var nOut = 0;
-
-        for (i = 0; i + filter.length - 1 < buffer.length; i = Math.round(samplingRateRatio * nOut)) {
-            var sample = 0;
-            for (var j = 0; j < filter.length; ++j) {
-                sample += buffer[i + j] * filter[j];
-            }
-            sample *= volume;
-            dataView16k.setInt16(index, sample, true); // 'true' -> little endian
-            index += 2;
-            nOut++;
-        }
-
-        var indexSampleAfterLastUsed = Math.round(samplingRateRatio * nOut);
-        var remaining = buffer.length - indexSampleAfterLastUsed;
-        if (remaining > 0) {
-            this.bufferUnusedSamples = new Float32Array(remaining);
-            for (i = 0; i < remaining; ++i) {
-                this.bufferUnusedSamples[i] = buffer[indexSampleAfterLastUsed + i];
-            }
-        } else {
-            this.bufferUnusedSamples = new Float32Array(0);
-        }
-
-        return new Blob([dataView16k], {type: 'audio/l16'});
-    };
+    return new Blob([dataView16k], {type: 'audio/l16'});
+  };
 }
